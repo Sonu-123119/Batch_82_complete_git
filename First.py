ca78f9b34f827f927b8939dac8baa8fbb167c008
Reshma6702 <reshamakale111@gmail.com>
2026-09-15 15:13:10 +0530

    my third commit

--- First.py
+++ First.py
@@ -1,19 +1,15 @@
+
+
 def fxadd(n1,n2,n3):
     return n1+n2+n3
-
 
 def fxsubs(n1,n2):
     return n1-n2
 
+def fx():
+    print("Hello good morning")
 
-def fxmultiply(n1,n2):
-    return n1*n2
 
-def fxdivide(n1,n2):
-    return n1/n2
-    
+fx()  
 print(fxsubs(5,3))
 print(fxadd(1,2,3))
-
-
-    
