6522c35d15202a926688dc214152cfd751bd9b0d
Sonu-123119 <sonukale6702@gmail.com>
2026-09-15 14:58:21 +0530

    My fellow developer commit
    Added functions for multiplication and division.

--- First.py
+++ First.py
@@ -4,5 +4,16 @@ def fxadd(n1,n2,n3):
 
 def fxsubs(n1,n2):
     return n1-n2
+
+
+def fxmultiply(n1,n2):
+    return n1*n2
+
+def fxdivide(n1,n2):
+    return n1/n2
+    
 print(fxsubs(5,3))
 print(fxadd(1,2,3))
+
+
+    
